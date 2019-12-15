76f12c604bef889f1358b3fa034f73f5044bb89e
e3c_100005333972_iven <e3c_100005333972_iven@git.cloud.tencent.com>
2019-12-15 11:24:17 +0800

    bug

--- src/service.ts
+++ src/service.ts
@@ -3,33 +3,32 @@ import { from, of } from "rxjs";
 import { config } from "./configuration";
 import { ServiceManagerService } from "cn-doscover-api";
 export class Service {
-  private _managerService: ServiceManagerService
-  constructor(){
+  private _managerService: ServiceManagerService;
+  constructor() {
     this._managerService = new ServiceManagerService();
   }
 
   async getNews(usid: string) {
     try {
-
-      return this._managerService.getNews(config.NewSBaseUrl,usid);
+      return await this._managerService.getNews(config.NewSBaseUrl, usid);
     } catch (error) {
       console.log(error);
     }
   }
 
-
-  async getServiceDiscover(query:string){
+  async getServiceDiscover(query: string) {
     console.log(query);
     try {
-      return this._managerService.getServiceDiscover(config.DiscoverService_URL,query);
+      return await this._managerService.getServiceDiscover(
+        config.DiscoverService_URL,
+        query
+      );
     } catch (error) {
       console.log(error);
     }
   }
-
-
 }
 
-export const sum = (a:number,b:number) => {
+export const sum = (a: number, b: number) => {
   return a + b;
 };
